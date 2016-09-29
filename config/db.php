29955ac154e5fa22a9997b3878f4aad6af176b20
kali0102 <kali.liu@qq.com>
2016-09-29 18:56:23 +0800

    基础
    基础

---
+++ config/db.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=127.0.0.1;dbname=yiiblog',
+    'username' => 'root',
+    'password' => '123456',
+    'charset' => 'utf8',
+    'tablePrefix' => 'tb_'
+];
---
+++ modules/admini/Module.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace app\modules\admini;
+
+/**
+ * admini module definition class
+ */
+class Module extends \yii\base\Module
+{
+    /**
+     * @inheritdoc
+     */
+    public $controllerNamespace = 'app\modules\admini\controllers';
+
+    /**
+     * @inheritdoc
+     */
+    public function init()
+    {
+        parent::init();
+
+        // custom initialization code goes here
+    }
+}
